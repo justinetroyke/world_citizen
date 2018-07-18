e5b43a2f3b4c8ab0363fa93e15c2a8539ad72c6d
Justine <jjtroyke@gmail.com>
2018-07-18 12:27:27 -0600

    item show page

--- spec/features/items/user_visits_item_registration_spec.rb
+++ spec/features/items/user_visits_item_registration_spec.rb
@@ -23,12 +23,13 @@ RSpec.describe 'User visits item#new' do
       click_on 'Submit'
 
       expect(current_path).to eq(item_path(Item.last.id))
-      expect(current_page).to have_content(biz)
-      expect(current_page).to have_content(name)
-      expect(current_page).to have_content(amt)
-      expect(current_page).to have_content(org)
-      expect(current_page).to have_content(org_address)
-      expect(current_page).to have_content(category)
+      save_and_open_page
+      expect(page).to have_content(biz)
+      expect(page).to have_content(name)
+      expect(page).to have_content(amt)
+      expect(page).to have_content(org)
+      expect(page).to have_content(org_address)
+      expect(page).to have_content(category)
     end
   end
 end
