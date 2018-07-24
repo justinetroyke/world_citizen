309928317c72e2b9df316d8a200545e0d458fd0a
Justine <jjtroyke@gmail.com>
2018-07-24 11:54:35 -0600

    got fetch working

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,9 @@
 class HomeController < ApplicationController
   def index
-    user_address = "1801 Chestnut Pl, Denver, CO"
-    start = user_address.gsub(',','+').sub(' ','+').gsub(' ','')
-    binding.pry
-    results = Faraday.get("https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=#{start}&destinations=737+Park+street+Piru+CA&key=AIzaSyBViIJ2f4FyRSr
-6dWGJXK46UPxsSlrb0pk")
     @items = Item.all
+    user_address = "1801 Chestnut Pl, Denver, CO"
+    start = user_address.sub(' ','+').gsub(',','+').sub(' ','+').gsub(' ','')
+    results = Faraday.get("https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=1801+Chestnut+Place+Denver+CO&destinations=737+Park+street+Piru+CA&key=AIzaSyBViIJ2f4FyRSr6dWGJXK46UPxsSlrb0pk")
+binding.pry
   end
 end
