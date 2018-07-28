45d6b050a3e02297d19c2ced05af3164be06e7cd
Justine <jjtroyke@gmail.com>
2018-07-28 14:58:26 -0600

    Successful intial API call

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,4 +1,7 @@
 class CampaignsController < ApplicationController
   def index
+    response = Faraday.get("https://api.donorschoose.org/common/json_feed.html?state=CA&APIKey=DONORSCHOOSE")
+    results = JSON.parse(response.body)
+    binding.pry
   end
 end
