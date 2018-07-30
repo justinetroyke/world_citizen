53538561015decb7e9c48c835834a944d9c0e587
Justine <jjtroyke@gmail.com>
2018-07-30 08:51:55 -0600

    adding test for collecting a stamp

--- spec/features/items/when_a_user_completes_item_purchase_spec.rb
+++ spec/features/items/when_a_user_completes_item_purchase_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
-describe 'returns closest stamps to address of user' do
-  context 'As a registered user I fill in address and click search' do
-    it 'returns closest stamp items to me' do
+describe 'rewards stamp to passport when item purchased' do
+  context 'As a registered user I complete item purchased' do
+    it 'awards a stamp to my passport' do
       fb = Category.create!(name: 'F&B')
       product = Category.create!(name: 'Product')
       fb.items.create!(business_name: 'Luna Gourmet Coffee & Tea Company', business_location: '7295 Washington St, Denver, CO 80229', name: 'Surfers for Autism Coffee', donation_amount: '$1', organization: 'Surfers for Autism', organization_location: '7491 N. Federal Hwy, Boca Raton FL 33487')
-      fb.items.create!(business_name: 'Hedge Row', business_location: '100 Steele St, Denver, CO 80206', name: 'cocktail booklet', donation_amount: 'proceeds', organization: 'The Park People', organization_location: '1510 S. Grant St. Denver, CO 80210')
+      item = fb.items.create!(business_name: 'Hedge Row', business_location: '100 Steele St, Denver, CO 80206', name: 'cocktail booklet', donation_amount: 'proceeds', organization: 'The Park People', organization_location: '1510 S. Grant St. Denver, CO 80210')
       product.items.create!(business_name: 'SameDay Office Supply', business_location: '7076 S Alton Way, Centennial, CO 80112', name: 'printer cartridge', donation_amount: 'portion of recycled cartridge', organization: 'Denver Rescue Mission', organization_location: '6100 Smith Road, Denver, CO, 80216')
       street = '1801 Chestnut Pl'
       city = 'Denver'
@@ -27,22 +27,32 @@ describe 'returns closest stamps to address of user' do
         select(state, from: 'state')
         # state box with "CO"
         click_on 'Search'
-        # and click "search"
       end
       expect(current_path).to eq(stamps_path)
-      # Then my current path should be "/"
-      # And I should see the 10 items with the closest business address to the submitted address
+
       within('.stamps') do
-        expect(page).to have_content('Luna Gourmet Coffee & Tea Company')
-        expect(page).to have_content('F&B')
-        expect(page).to have_content('Local')
-        expect(page).to have_content('Surfers for Autism Coffee')
-        expect(page).to have_content('Surfers for Autism')
-        expect(page).to have_content('4.9 mi')
-        # And I should see the business name, category, stamp, item, charity org and distance to item business address for each of the 10 results
+        click_on('cocktail booklet')
       end
+      # click on an item
+      expect(current_path).to eq(item_path(item.id))
+
+      expect(page).to have_content(item.business_name)
+      expect(page).to have_content(item.business_location)
+      expect(page).to have_content(item.name)
+      expect(page).to have_content(item.donation_amount)
+      expect(page).to have_content(item.organization)
+      expect(page).to have_content(item.organization_location)
+      expect(page).to have_content('4.9 mi')
+
+      
     end
   end
 end
 
-# I should see a page links to see more results
+# my current path is "stamp/show"
+# I see all the item details
+# I click on item purchased
+# My current path is user_passport_path
+# I see a message that says "You've earned #{level} Stamp! Congratulations"
+# I should see a second message that says
+# "Great job giving back! In a mood to give more? Click HERE to collect double the stamps for your passport by directly sponsoring a DonorChose campaign!"
