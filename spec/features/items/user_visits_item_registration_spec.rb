f6b509631fbe33018bb359f32b8bc45429c86465
Justine <jjtroyke@gmail.com>
2018-07-31 14:33:57 -0600

    Fixed new item test

--- spec/features/items/user_visits_item_registration_spec.rb
+++ spec/features/items/user_visits_item_registration_spec.rb
@@ -1,28 +1,41 @@
 require 'rails_helper'
 
-RSpec.describe 'User visits item#new' do
+RSpec.describe 'Registered User creates a new item' do
   context 'user registers a new item' do
-    xit 'should create item and direct to item show' do
-      biz = "BJ’s Restaurants"
-      name = 'Pizookie'
-      amt = '10'
-      org = 'Cystic Fibrosis Foundation'
+    it 'should create item and direct to item show' do
+      add = "Create New Item"
+      category = Category.create!(name: 'F&B').name
+      biz = "Bob's Burgers"
+      biz_loc = '10446 Town Center Dr, Westminster, CO 80021'
+      name = 'Triple Decker'
+      amt = '$10'
+      org = 'Less Burgers'
       org_address = '4550 Montgomery Ave., Suite 1100 N, Bethesda, MD 20814'
-      category = "F&B"
-      # user = User.create!(name: 'Inigo Montoya', email: 'preparetodie@father.com', password: 'killed')
 
-      visit new_item_path
+      user = User.create(name: 'Burt Macklin', email: 'stunna@fbi.com', password: '123abc')
+      user.passports.create!
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit stamps_path
+
+      click_on(add)
+
+      expect(current_path).to eq(new_item_path)
 
       fill_in 'item[business_name]', with: biz
+      fill_in 'item[business_location]', with: biz_loc
       fill_in 'item[name]', with: name
       fill_in 'item[donation_amount]', with: amt
       fill_in 'item[organization]', with: org
       fill_in 'item[organization_location]', with: org_address
-      select(category, from: 'Category')
-      click_on 'Submit'
+      select(category, from: 'item_category')
+      click_on 'Create New Item'
 
-      expect(current_path).to eq(item_path(Item.last.id))
+      item = Item.last
+      expect(current_path).to eq(item_path(item.id))
       expect(page).to have_content(biz)
+      expect(page).to have_content(biz_loc)
       expect(page).to have_content(name)
       expect(page).to have_content(amt)
       expect(page).to have_content(org)
