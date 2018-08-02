e9942837720c4fcdc2ad76eccdd6614afb052e4b
Justine <jjtroyke@gmail.com>
2018-08-02 12:23:21 -0600

    updated stamp calculation to model

--- app/controllers/passports_controller.rb
+++ app/controllers/passports_controller.rb
@@ -3,13 +3,13 @@ class PassportsController < ApplicationController
     @user = current_user
     @passport = Passport.find_by(user_id: @user.id)
     @stamp = params['stamp']
-    @stamps = @passport.passport_stamps
     if @stamp
       stamp_id = Stamp.find_by(name: @stamp).id
       @passport.passport_stamps.create!(stamp_id: stamp_id)
       flash[:success] = "Congratulations! You've earned a #{@stamp} Stamp for your Passport!"
       flash[:notice] = "Great job giving back! In a mood to give more? To directly sponsor a DonorsChoose campaign and collect double the stamps for your passport click #{view_context.link_to 'here', campaigns_path}!".html_safe
     end
+    @stamps = @passport.stamp_amounts(@passport)
   end
 
 private
--- app/models/passport.rb
+++ app/models/passport.rb
@@ -1,6 +1,37 @@
 class Passport < ApplicationRecord
   belongs_to :user
-  
+
   has_many :passport_stamps
   has_many :stamps, through: :passport_stamps
+
+  def stamp_amounts(passport)
+    find_amounts(passport.passport_stamps).first
+  end
+private
+
+  def find_amounts(stamps)
+    amounts = {
+          local: 0,
+          district: 0,
+          regional: 0,
+          national: 0,
+          international: 0,
+        }
+    stamps.map do |stamp|
+      stamp_object = Stamp.find_by(id: stamp.stamp_id)
+      stamp_name = stamp_object.name
+      if stamp_name == 'Local'
+        amounts[:local] += 1
+      elsif stamp_name == 'District'
+        amounts[:district] += 1
+      elsif stamp_name == 'Regional'
+        amounts[:regional] += 1
+      elsif stamp_name == 'National'
+        amounts[:national] += 1
+      elsif stamp_name == 'International'
+        amounts[:international] += 1
+      end
+      amounts
+    end
+  end
 end
