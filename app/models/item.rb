cc19d99f7d6ce3e071edde5c6e42996e4e022a71
Justine <jjtroyke@gmail.com>
2018-10-14 14:17:11 -0600

    Removed old code

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,14 +11,6 @@ class Item < ApplicationRecord
 
   belongs_to :category
 private
-  # def format_org_location
-  #   self.organization_location = google_address(self.organization_location)
-  # end
-  #
-  # def google_address(location)
-  #   location.sub(' ','+').gsub(',','+').sub(' ','+').gsub(' ','')
-  # end
-
   def lat_lng
     ItemGeocoderService.new(self).update
     ItemGeocoderService.new(self).update_org
