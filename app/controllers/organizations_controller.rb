ba373f738d20c1ed41a9fec7f680cc2f86bec9b7
Justine <jjtroyke@gmail.com>
2019-01-05 20:16:32 -0700

    New item process through to biz, org and starting item

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -3,26 +3,26 @@ class OrganizationsController < ApplicationController
     @organization = Organization.new
   end
 
-  # def create
-  #   @organization = Organization.create!(organization_params)
-  #   if @organization.save
-  #     flash[:success] = "#{@organization.name} has been added!"
-  #
-  #     redirect_to new_organization_path
-  #   else
-  #     flash[:error] = "Please complete all fields"
-  #
-  #     redirect_to new_organization_path
-  #   end
-  # end
+  def create
+    @organization = Organization.create!(organization_params)
+    if @organization.save
+      flash[:success] = "#{@organization.name} has been added!"
+
+      redirect_to new_item_path
+    else
+      flash[:error] = "Please complete all fields"
+
+      redirect_to new_organization_path
+    end
+  end
 #
 #   def show
 #     @organization = Organization.find(params[:id])
 #   end
-# private
-#   def organization_params
-#     params.require(:organization).permit(
-#                                  :name,
-#                                  :location)
-#   end
+private
+  def organization_params
+    params.require(:organization).permit(
+                                 :name,
+                                 :location)
+  end
 end
--- spec/features/items/user_registers_item_spec.rb
+++ spec/features/items/user_registers_item_spec.rb
@@ -7,6 +7,8 @@ RSpec.describe 'Registered User creates a new item' do
       category = Category.create!(name: 'F&B').name
       biz = "Bob's Burgers"
       biz_loc = '10446 Town Center Dr, Westminster, CO 80021'
+      biz_lat = '39.8881213'
+      biz_lng = '-105.0742362'
       name = 'Triple Decker'
       amt = '$10'
       org = 'Less Burgers'
@@ -22,18 +24,27 @@ RSpec.describe 'Registered User creates a new item' do
       click_on(add)
 
       expect(current_path).to eq(new_business_path)
+
       fill_in 'business[name]', with: biz
       fill_in 'business[location]', with: biz_loc
       click_on 'Save New Business'
-      # business = Business.last
-      # expect(business.name).to eq(biz)
-      # expect(business.location).to eq(biz_loc)
-      #
-      # fill_in 'item[name]', with: name
-      # fill_in 'item[donation_amount]', with: amt
-      # select(category, from: 'item_category')
-      # click_on 'Create New Item'
-      # binding.pry
+      business = Business.last
+      expect(business.name).to eq(biz)
+      expect(business.location).to eq(biz_loc)
+      expect(business.latitude).to eq(biz_lat)
+      expect(business.longitude).to eq(biz_lng)
+
+      expect(current_path).to eq(new_organization_path)
+
+      fill_in 'organization[name]', with: org
+      fill_in 'organization[location]', with: org_address
+      click_on 'Add Organization'
+      expect(current_path).to eq(new_item_path)
+
+      fill_in 'item[name]', with: name
+      fill_in 'item[donation_amount]', with: amt
+      select(category, from: 'item_category')
+      click_on 'Create New Item'
       # item = Item.last
       # expect(item.name).to eq(name)
       # expect(item.donation_amount).to eq(amt)
@@ -43,8 +54,7 @@ RSpec.describe 'Registered User creates a new item' do
       # expect(updated_item.category.name).to eq(category)
       # expect(updated_item.business_id).to eq(business.id)
 
-      # fill_in 'item[organization]', with: org
-      # fill_in 'item[organization_location]', with: org_address
+
       # expect(page).to have_content(biz)
       # expect(page).to have_content(biz_loc)
       # expect(page).to have_content(name)
