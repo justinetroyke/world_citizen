020e0647cd82a5710ff7020911528d9385bcc306
Justine <jjtroyke@gmail.com>
2019-01-05 19:52:48 -0700

    Biz and org testing views

---
+++ app/controllers/organizations_controller.rb
@@ -0,0 +1,28 @@
+class OrganizationsController < ApplicationController
+  def new
+    @organization = Organization.new
+  end
+
+  # def create
+  #   @organization = Organization.create!(organization_params)
+  #   if @organization.save
+  #     flash[:success] = "#{@organization.name} has been added!"
+  #
+  #     redirect_to new_organization_path
+  #   else
+  #     flash[:error] = "Please complete all fields"
+  #
+  #     redirect_to new_organization_path
+  #   end
+  # end
+#
+#   def show
+#     @organization = Organization.find(params[:id])
+#   end
+# private
+#   def organization_params
+#     params.require(:organization).permit(
+#                                  :name,
+#                                  :location)
+#   end
+end
