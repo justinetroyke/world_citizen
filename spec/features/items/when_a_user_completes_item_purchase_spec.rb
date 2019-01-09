dac217494b44a4a68b8d15357b592cab7aa25d3b
Justine <jjtroyke@gmail.com>
2019-01-08 23:25:32 -0700

    WIP when a user completes purchase test

--- spec/features/items/when_a_user_completes_item_purchase_spec.rb
+++ spec/features/items/when_a_user_completes_item_purchase_spec.rb
@@ -5,9 +5,12 @@ describe 'rewards stamp to passport when item purchased' do
     it 'awards a stamp to my passport' do
       fb = Category.create!(name: 'F&B')
       product = Category.create!(name: 'Product')
-      fb.items.create!(business_name: 'Luna Gourmet Coffee & Tea Company', business_location: '7295 Washington St, Denver, CO 80229', name: 'Surfers for Autism Coffee', donation_amount: '$1', organization: 'Surfers for Autism', organization_location: '7491 N. Federal Hwy, Boca Raton FL 33487')
-      item = fb.items.create!(business_name: 'Hedge Row', business_location: '100 Steele St, Denver, CO 80206', name: 'cocktail booklet', donation_amount: 'proceeds', organization: 'The Park People', organization_location: '1510 S. Grant St. Denver, CO 80210')
-      product.items.create!(business_name: 'SameDay Office Supply', business_location: '7076 S Alton Way, Centennial, CO 80112', name: 'printer cartridge', donation_amount: 'portion of recycled cartridge', organization: 'Denver Rescue Mission', organization_location: '6100 Smith Road, Denver, CO, 80216')
+      biz = Business.create!(name: 'Luna Gourmet Coffee & Tea Company', location: '7295 Washington St, Denver, CO 80229')
+      biz3 = Business.create!(name: 'ABC', location: '1510 S. Grant St., Denver, CO 80206')
+      org = Organization.create!(name: 'Surfers for Autism', location: '7491 N. Federal Hwy, Boca Raton FL 33487')
+      org3 = Organization.create!(name: 'edf', location: '1510 S. Grant St. Denver, CO 80210')
+      fb.items.create!(name: 'Surfers for Autism Coffee', donation_amount: '$1', business_id: biz.id, organization_id: org.id)
+      item = product.items.create!(name: 'printer cartridge', donation_amount: 'portion of recycled cartridge', business_id: biz3.id, organization_id: org3.id)
       street = '1801 Chestnut Pl'
       city = 'Denver'
       state = "CO"
@@ -33,18 +36,19 @@ describe 'rewards stamp to passport when item purchased' do
       expect(current_path).to eq(stamps_path)
 
       within('.stamps') do
-        click_on('cocktail booklet')
+        click_on('printer cartridge')
       end
       # click on an item
       expect(current_path).to eq("/stamps/#{item.id}")
       # my current path is "item/show"
 
-      expect(page).to have_content(item.business_name)
-      expect(page).to have_content(item.business_location)
+      save_and_open_page
+      expect(page).to have_content(item.business.name)
+      expect(page).to have_content(item.business.location)
       expect(page).to have_content(item.category.name)
       expect(page).to have_content(item.name)
       expect(page).to have_content(item.donation_amount)
-      expect(page).to have_content(item.organization)
+      expect(page).to have_content(item.organization.name)
       # I see all the item details
       click_on('Item Purchased')
       # I click on item purchased
